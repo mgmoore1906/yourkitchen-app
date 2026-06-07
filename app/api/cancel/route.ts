7ee2b92db2ce00cde29444cbb0b1b0f0745d7f60
mgmoore1906 <143183149+mgmoore1906@users.noreply.github.com>
2026-06-06 20:25:01 -0500

    Update route.ts

--- app/api/cancel/route.ts
+++ app/api/cancel/route.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import Stripe from 'stripe'
 import twilio from 'twilio'
 import { NextResponse } from 'next/server'
-
+export const dynamic = 'force-dynamic'
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
